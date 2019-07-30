dcd081d8ef92c9d33817f050e3306d6c34c4fb21
dinesh5492sun <dinesh5492sun@gmail.com>
2019-07-30 16:05:55 +0530

    added one more sysout

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Running HelloWorld");
+		System.out.println("Running HelloWorld"); 
+		System.out.println("successful Running");
 	}
 }
